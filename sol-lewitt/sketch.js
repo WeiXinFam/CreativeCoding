04dcb923acd6749075b5f878b5724ca7f1a48975
WeiXin Fam <wxinfam18@gmail.com>
2021-08-27 08:50:32 +1000

    ✨ Fix length for vertical and horizontal

--- sol-lewitt/sketch.js
+++ sol-lewitt/sketch.js
@@ -12,19 +12,12 @@ function setup() {
 }
 
 function draw() {
-  // recursiveDrawHorizontal(0, 0);
-  // recursiveDrawHorizontal(0, height - rectWidth);
-  // recursiveDrawHorizontal(rectWidth, rectWidth);
-  // recursiveDrawHorizontal(rectWidth, height - 2 * rectWidth);
-  // recursiveDrawVertical(0, rectWidth);
-  // recursiveDrawVertical(width - rectWidth, 0);
-  // recursiveDrawVertical(rectWidth, 2 * rectWidth);
-  // recursiveDrawVertical(width - 2 * rectWidth, rectWidth);
   let i = 0;
-  let j = 0;
-  while (i < 5) {
+  while (i < 2) {
     recursiveDrawHorizontal(rectWidth * i, rectWidth * i);
     recursiveDrawHorizontal(rectWidth * i, height - (i + 1) * rectWidth);
+    recursiveDrawVertical(rectWidth * i, rectWidth * (i + 1));
+    recursiveDrawVertical(width - rectWidth * (i + 1), rectWidth * i);
     i++;
   }
   noLoop();
@@ -32,25 +25,32 @@ function draw() {
 
 function recursiveDrawHorizontal(posX, posY) {
   let randLth;
-  let continuedX;
   let initalPosX = posX;
   while (posX < width - initalPosX) {
-    if (posX == 0) {
-      randLth = randomLength(initialLength, rectWidth);
-      recursiveFirstRow(posX, posY, randLth, rectWidth);
-      posX += randLth;
-    }
     randLth = randomLength(initialLength, rectWidth);
-    // console.log("continued " + continuedX);
+
+    // Ensures the width is exactly as intended for
+    if (posX + randLth > width - initalPosX) {
+      randLth = width - initalPosX - posX;
+    }
+
     recursiveFirstRow(posX, posY, randLth, rectWidth);
     posX += randLth;
   }
 }
 
 function recursiveDrawVertical(posX, posY) {
-  while (posY < height - rectWidth) {
+  let randLth;
+  let initalPosY = posY;
+  while (posY < height - initalPosY) {
     randLth = randomLength(initialLength, rectWidth);
-    recursiveFirstRow(posX, posY, rectWidth, initialLength);
+
+    // Ensures the width is exactly as intended for
+    if (posY + randLth > height - initalPosY) {
+      randLth = height - initalPosY - posY;
+    }
+
+    recursiveFirstRow(posX, posY, rectWidth, randLth);
     posY += randLth;
   }
 }
