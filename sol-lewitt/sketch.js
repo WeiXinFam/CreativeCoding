2358039f254745613fcb01d0254aea6e699c7c03
WeiXin Fam <wxinfam18@gmail.com>
2021-08-30 23:23:25 +1000

    🚧 Points around shape

--- sol-lewitt/sketch.js
+++ sol-lewitt/sketch.js
@@ -1,20 +1,32 @@
-let spacing = 10;
+let spacing = 3;
 function setup() {
   createCanvas(800, 800);
-  background(0, 0, 0);
+  // background(0, 0, 0);
 }
 
 function draw() {
   circle(width / 2, height / 2, width / 2);
   stroke(0);
   let i = 0;
-  while (i < 20) {
+  while (i < 68) {
     let posXStart = width / 2 - spacing * i;
     let posXEnd = width / 2 + spacing * i;
-    line(posXStart, height / 2 + 20, posXStart, height / 2 - 20);
-    line(posXEnd, height / 2 + 20, posXEnd, height / 2 - 20);
+    let posYBottom = height / 2 + width / 4;
+    let posYStart = height / 2 - width / 4;
+    line(posXStart, posYStart, posXStart, posYBottom);
+    line(posXEnd, posYStart, posXEnd, posYBottom);
     i++;
     console.log(i);
   }
+  quad(
+    width / 2 - 80,
+    height / 2 - 60,
+    width / 2,
+    height / 2 - 60,
+    width / 2 + 40,
+    height / 2 + 60,
+    width / 2 - 40,
+    height / 2 + 60
+  );
   noLoop();
 }
